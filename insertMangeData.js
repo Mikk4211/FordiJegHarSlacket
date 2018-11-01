3629ca7de0b1f1529d52f2b7c3c3491aebc0cf87
mikk4211 <mikk4211@edu.easj.dk>
2018-11-01 15:40:18 +0100

    Indsat mange data

---
+++ insertMangeData.js
@@ -0,0 +1,19 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var myobj = [
+    { name: 'John', address: 'Highway 71'},
+    { name: 'Peter', address: 'Lowstreet 4'},
+    { name: 'Amy', address: 'Apple st 652'}
+  ];
+  // Forskellen mellem insertOne og insertMany ligger lidt i navnet.
+  // Den ene kan kun indsætte en, mens den anden kan indsætte flere ting til en collection. 
+  dbo.collection("customers").insertMany(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("Number of documents inserted: " + res.insertedCount);
+    db.close();
+  });
+});
